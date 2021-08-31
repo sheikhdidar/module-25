370ffda58434241c41e5eb37fa498756930cdde5
sheikhdidar <sheikhdidar67@gmail.com>
2021-08-31 06:16:09 +0600

    first-commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('submit-button').addEventListener('click', function(){
+            
+    const emailInput = document.getElementById('email-input');
+    const userEmail = emailInput.value;
+    emailInput.value = '';
+    
+    const passInput = document.getElementById('pass-input');
+    const userPassword = passInput.value;
+    passInput.value = '';
+    
+    if(userEmail == 'sontan@gmail.com' && userPassword == 'secrete'){
+        window.location.href = 'banking.html';
+    }
+
+})
